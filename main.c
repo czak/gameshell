bc5f52a2867f7258aafb941f0df73fbad6d6148f
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2024-06-02 15:29:29 +0200

    Minimal wayland-egl client

--- main.c
+++ main.c
@@ -4,13 +4,18 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <wayland-client.h>
+#include <wayland-egl.h>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
 
 #include "protocols/xdg-shell.h"
+
+const int width = 256;
+const int height = 256;
 
 static struct wl_display *wl_display;
 static struct wl_registry *wl_registry;
 
-static struct wl_shm *wl_shm;
 static struct wl_compositor *wl_compositor;
 static struct xdg_wm_base *xdg_wm_base;
 
@@ -18,7 +23,10 @@ static struct wl_surface *wl_surface;
 static struct xdg_surface *xdg_surface;
 static struct xdg_toplevel *xdg_toplevel;
 
-static struct wl_buffer *wl_buffer;
+static struct wl_egl_window *wl_egl_window;
+static EGLDisplay egl_display;
+static EGLContext egl_context;
+static EGLSurface egl_surface;
 
 static void noop() {}
 
@@ -26,11 +34,7 @@ static void wl_registry_global(void *data, struct wl_registry *wl_registry,
 		uint32_t name, const char *interface, uint32_t version)
 {
 	// clang-format off
-	if (strcmp(interface, wl_shm_interface.name) == 0) {
-		wl_shm = wl_registry_bind(wl_registry, name, &wl_shm_interface, 1);
-	}
-
-	else if (strcmp(interface, wl_compositor_interface.name) == 0) {
+	if (strcmp(interface, wl_compositor_interface.name) == 0) {
 		wl_compositor = wl_registry_bind(wl_registry, name, &wl_compositor_interface, 4);
 	}
 
@@ -49,39 +53,45 @@ static void xdg_surface_configure(void *data, struct xdg_surface *xdg_surface,
 		uint32_t serial)
 {
 	xdg_surface_ack_configure(xdg_surface, serial);
-
-	wl_surface_attach(wl_surface, wl_buffer, 0, 0);
-	wl_surface_commit(wl_surface);
 }
 
 static const struct xdg_surface_listener xdg_surface_listener = {
 	.configure = xdg_surface_configure,
 };
 
-void init_buffer()
+static void init_egl_window()
 {
-	const int width = 256;
-	const int height = 256;
+	const EGLint config_attributes[] = {
+		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+		EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+		EGL_RED_SIZE, 8,
+		EGL_GREEN_SIZE, 8,
+		EGL_BLUE_SIZE, 8,
+		EGL_ALPHA_SIZE, 8,
+		EGL_NONE,
+	};
 
-	const int stride = width * sizeof(uint32_t); // 4 bytes per
-	const int size = stride * height;
+	const EGLint context_attributes[] = {
+		EGL_CONTEXT_MAJOR_VERSION, 2,
+		EGL_NONE
+	};
 
-	int fd = memfd_create("buffer-pool", 0);
-	ftruncate(fd, size);
+	EGLConfig egl_config = NULL;
+	EGLint num_configs = 0;
 
-	uint32_t *pixels = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
-			pixels[width * y + x] = 0x3f00ff3f;
-		}
-	}
-	munmap(pixels, size);
+	egl_display = eglGetDisplay(wl_display);
+	eglInitialize(egl_display, NULL, NULL);
+	eglChooseConfig(egl_display, config_attributes, &egl_config, 1, &num_configs);
 
-	struct wl_shm_pool *wl_shm_pool = wl_shm_create_pool(wl_shm, fd, size);
-	wl_buffer = wl_shm_pool_create_buffer(wl_shm_pool, 0, width, height, stride, WL_SHM_FORMAT_ARGB8888);
-	wl_shm_pool_destroy(wl_shm_pool);
+	assert(egl_display && egl_config && num_configs == 1);
 
-	close(fd);
+	egl_context = eglCreateContext(egl_display, egl_config, EGL_NO_CONTEXT, context_attributes);
+
+	assert(egl_context);
+
+	wl_egl_window = wl_egl_window_create(wl_surface, width, height);
+	egl_surface = eglCreateWindowSurface(egl_display, egl_config, wl_egl_window, NULL);
+	eglMakeCurrent(egl_display, egl_surface, egl_surface, egl_context);
 }
 
 int main(int argc, char *argv[])
@@ -91,7 +101,7 @@ int main(int argc, char *argv[])
 	wl_registry_add_listener(wl_registry, &wl_registry_listener, NULL);
 	wl_display_roundtrip(wl_display);
 
-	assert(wl_shm && wl_compositor && xdg_wm_base);
+	assert(wl_compositor && xdg_wm_base);
 
 	wl_surface = wl_compositor_create_surface(wl_compositor);
 	xdg_surface = xdg_wm_base_get_xdg_surface(xdg_wm_base, wl_surface);
@@ -99,13 +109,19 @@ int main(int argc, char *argv[])
 
 	assert(wl_surface && xdg_surface && xdg_toplevel);
 
-	init_buffer();
+	init_egl_window();
 
-	assert(wl_buffer);
+	assert(wl_egl_window && egl_surface);
 
 	xdg_surface_add_listener(xdg_surface, &xdg_surface_listener, NULL);
 	wl_surface_commit(wl_surface);
 
+	fprintf(stderr, "%s\n", glGetString(GL_VERSION));
+
 	while (wl_display_dispatch(wl_display) != -1) {
+		glClearColor(0.3f, 0.0f, 0.0f, 0.3f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		eglSwapBuffers(egl_display, egl_surface);
 	}
 }
