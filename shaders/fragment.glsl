95d8523b29b60f434090b8bf83b6d2cc3dfceb93
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2024-06-08 14:10:12 +0200

    Premultiply text color by alpha

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -7,5 +7,5 @@ varying vec2 v_TexCoord;
 void main()
 {
   float a = texture2D(u_Sampler, v_TexCoord).a;
-  gl_FragColor = vec4(1.0, 0.75, 0.3, a);
+  gl_FragColor = vec4(1.0 * a, 0.75 * a, 0.3 * a, a);
 }
