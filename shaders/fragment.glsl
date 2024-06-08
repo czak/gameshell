2e2764af0d56f487fe2b1377e28bb4fccd5fe4b2
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2024-06-08 19:54:45 +0200

    Configurable shader source

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,12 +1,21 @@
 precision mediump float;
 
+#define SHADER_ID_IMAGE 0
+#define SHADER_ID_SOLID 1
+
 uniform sampler2D u_Sampler;
 uniform vec3 u_Color;
+uniform int u_ShaderId;
 
 varying vec2 v_TexCoord;
 
 void main()
 {
-  float a = texture2D(u_Sampler, v_TexCoord).a;
-  gl_FragColor = vec4(u_Color.xyz, a);
+  vec4 texColor = texture2D(u_Sampler, v_TexCoord);
+
+  if (u_ShaderId == SHADER_ID_SOLID) {
+    texColor = vec4(u_Color.xyz, texColor.a);
+  }
+
+  gl_FragColor = texColor;
 }
