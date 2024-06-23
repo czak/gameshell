e095f83a48c5e8896539d2b517eaacf8ecb89b9f
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2024-06-23 18:57:42 +0200

    Fix duplication

--- main.c
+++ main.c
@@ -64,24 +64,6 @@ static void on_continue()
 	kill(-selected_command()->pid, SIGCONT);
 }
 
-void menu_hover_next_item(struct menu *menu)
-{
-	if (menu->hover < menu->items_count - 1) menu->hover++;
-}
-
-void menu_hover_prev_item(struct menu *menu)
-{
-	if (menu->hover > 0) menu->hover--;
-}
-
-void menu_trigger_item(struct menu *menu)
-{
-	struct menu_item *hover_menu_item = &menu->items[menu->hover];
-	if (hover_menu_item->action) {
-		hover_menu_item->action(hover_menu_item->data);
-	}
-}
-
 static void draw_menu(struct menu *menu, int px, int py)
 {
 	struct color c;
