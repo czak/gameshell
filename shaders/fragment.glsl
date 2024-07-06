bda6df1d95e13fe72d5ff78d8361b234e1b04b67
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2024-07-06 14:20:38 +0200

    MSDF fix: ensure screenPxRange is at least 1.0

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -19,8 +19,13 @@ void main()
   vec3 sample = texture2D(u_Sampler, v_TexCoord).rgb;
   float sigDist = median(sample.r, sample.g, sample.b) - 0.5;
 
-  // see https://github.com/Chlumsky/msdfgen/issues/36#issuecomment-429240110
-  sigDist *= u_Scale * u_ViewportRatio * FONT_PXRANGE / FONT_SIZE;
+  // see:
+  // - https://github.com/Chlumsky/msdfgen/blob/master/README.md#using-a-multi-channel-distance-field
+  // - https://github.com/Chlumsky/msdfgen/issues/36#issuecomment-429240110
+  //
+  // note: screenPxRange must never be lower than 1
+  float screenPxRange = max(1.0, u_Scale * u_ViewportRatio * FONT_PXRANGE / FONT_SIZE);
+  sigDist *= screenPxRange;
 
   float alpha = clamp(sigDist + 0.5, 0.0, 1.0);
   gl_FragColor = vec4(u_Color.rgb, u_Color.a * alpha);
