3bde4e5841b4e68c01bfa5daefd56715b0ccdf6d
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-01-02 11:30:17 +0100

    Use NDEBUG instead of custom define

--- log.h
+++ log.h
@@ -2,8 +2,8 @@
 
 void log_print(const char *file, int lineno, const char *fmt, ...);
 
-#if defined(_LOG_ENABLE) && _LOG_ENABLE
-	#define LOG(...) log_print(__FILE__, __LINE__, __VA_ARGS__);
-#else
+#ifdef NDEBUG
 	#define LOG(...)
+#else
+	#define LOG(...) log_print(__FILE__, __LINE__, __VA_ARGS__);
 #endif
