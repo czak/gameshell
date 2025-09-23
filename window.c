b6c7d5ae2fcb14297c335e4d9e1bd78446c02c2b
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-09-23 11:07:54 +0200

    Reformat window.c

--- window.c
+++ window.c
@@ -44,8 +44,8 @@ static struct {
 	int visible;
 } window;
 
-static void wl_registry_global(void *data, struct wl_registry *wl_registry,
-		uint32_t name, const char *interface, uint32_t version)
+static void wl_registry_global(void *data, struct wl_registry *wl_registry, uint32_t name,
+		const char *interface, uint32_t version)
 {
 	// clang-format off
 	if (strcmp(interface, wl_compositor_interface.name) == 0) {
@@ -80,38 +80,42 @@ static const struct wl_registry_listener wl_registry_listener = {
 	.global_remove = wl_registry_global_remove,
 };
 
-static void wl_keyboard_keymap(void *data, struct wl_keyboard *wl_keyboard, uint32_t format, int32_t fd, uint32_t size)
+static void wl_keyboard_keymap(void *data, struct wl_keyboard *wl_keyboard, uint32_t format,
+		int32_t fd, uint32_t size)
 {
 	// nop
 }
 
-static void wl_keyboard_enter(void *data, struct wl_keyboard *wl_keyboard, uint32_t serial, struct wl_surface *surface, struct wl_array *keys)
+static void wl_keyboard_enter(void *data, struct wl_keyboard *wl_keyboard, uint32_t serial,
+		struct wl_surface *surface, struct wl_array *keys)
 {
 	// nop
 }
 
-static void wl_keyboard_leave(void *data, struct wl_keyboard *wl_keyboard, uint32_t serial, struct wl_surface *surface)
+static void wl_keyboard_leave(void *data, struct wl_keyboard *wl_keyboard, uint32_t serial,
+		struct wl_surface *surface)
 {
 	// nop
 }
 
-static void wl_keyboard_key(void *data, struct wl_keyboard *wl_keyboard,
-		uint32_t serial, uint32_t time, uint32_t key, uint32_t state)
+static void wl_keyboard_key(void *data, struct wl_keyboard *wl_keyboard, uint32_t serial,
+		uint32_t time, uint32_t key, uint32_t state)
 {
-	if (state != WL_KEYBOARD_KEY_STATE_PRESSED) return;
+	if (state != WL_KEYBOARD_KEY_STATE_PRESSED)
+		return;
 
 	if (window.on_key)
 		window.on_key(key);
 }
 
-static void wl_keyboard_modifiers(void *data, struct wl_keyboard *wl_keyboard, uint32_t serial, uint32_t mods_depressed,
-								  uint32_t mods_latched, uint32_t mods_locked, uint32_t group)
+static void wl_keyboard_modifiers(void *data, struct wl_keyboard *wl_keyboard, uint32_t serial,
+		uint32_t mods_depressed, uint32_t mods_latched, uint32_t mods_locked, uint32_t group)
 {
 	// nop
 }
 
 static void wl_keyboard_repeat_info(void *data, struct wl_keyboard *wl_keyboard, int32_t rate,
-									int32_t delay)
+		int32_t delay)
 {
 	// nop
 }
@@ -126,15 +130,17 @@ static const struct wl_keyboard_listener wl_keyboard_listener = {
 };
 
 static void zwlr_layer_surface_v1_configure(void *data,
-		struct zwlr_layer_surface_v1 *zwlr_layer_surface_v1, uint32_t serial,
-		uint32_t width, uint32_t height)
+		struct zwlr_layer_surface_v1 *zwlr_layer_surface_v1, uint32_t serial, uint32_t width,
+		uint32_t height)
 {
 	zwlr_layer_surface_v1_ack_configure(window.zwlr_layer_surface_v1, serial);
 
 	window.visible = 1;
 
-	if (width > 0) window.width = width;
-	if (height > 0) window.height = height;
+	if (width > 0)
+		window.width = width;
+	if (height > 0)
+		window.height = height;
 
 	wl_egl_window_resize(window.wl_egl_window, window.width, window.height, 0, 0);
 
@@ -164,19 +170,22 @@ static const struct zwlr_layer_surface_v1_listener zwlr_layer_surface_v1_listene
 static void egl_init()
 {
 	const EGLint config_attributes[] = {
-		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-		EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
-		EGL_RED_SIZE, 8,
-		EGL_GREEN_SIZE, 8,
-		EGL_BLUE_SIZE, 8,
-		EGL_ALPHA_SIZE, 8,
+		EGL_SURFACE_TYPE,
+		EGL_WINDOW_BIT,
+		EGL_RENDERABLE_TYPE,
+		EGL_OPENGL_ES2_BIT,
+		EGL_RED_SIZE,
+		8,
+		EGL_GREEN_SIZE,
+		8,
+		EGL_BLUE_SIZE,
+		8,
+		EGL_ALPHA_SIZE,
+		8,
 		EGL_NONE,
 	};
 
-	const EGLint context_attributes[] = {
-		EGL_CONTEXT_MAJOR_VERSION, 2,
-		EGL_NONE
-	};
+	const EGLint context_attributes[] = {EGL_CONTEXT_MAJOR_VERSION, 2, EGL_NONE};
 
 	EGLint num_configs = 0;
 
@@ -184,37 +193,49 @@ static void egl_init()
 	eglInitialize(egl.display, NULL, NULL);
 	eglChooseConfig(egl.display, config_attributes, &egl.config, 1, &num_configs);
 
-	if (!egl.display) log_fatal("eglGetDisplay failed");
-	if (!egl.config || num_configs != 1) log_fatal("eglChooseConfig failed");
+	if (!egl.display)
+		log_fatal("eglGetDisplay failed");
+	if (!egl.config || num_configs != 1)
+		log_fatal("eglChooseConfig failed");
 
 	egl.context = eglCreateContext(egl.display, egl.config, EGL_NO_CONTEXT, context_attributes);
 
-	if (!egl.context) log_fatal("Failed to create EGL rendering context");
+	if (!egl.context)
+		log_fatal("Failed to create EGL rendering context");
 
 	window.wl_egl_window = wl_egl_window_create(window.wl_surface, 200, 200);
-	window.egl_surface = eglCreateWindowSurface(egl.display, egl.config, window.wl_egl_window, NULL);
+	window.egl_surface = eglCreateWindowSurface(egl.display, egl.config, window.wl_egl_window,
+			NULL);
 
-	if (!window.egl_surface) log_fatal("Failed to create EGL rendering surface");
+	if (!window.egl_surface)
+		log_fatal("Failed to create EGL rendering surface");
 
 	eglMakeCurrent(egl.display, window.egl_surface, window.egl_surface, egl.context);
 }
 
-void window_init(void (*on_draw)(), void (*on_resize)(int width, int height), void (*on_key)(int key))
+void window_init(void (*on_draw)(), void (*on_resize)(int width, int height),
+		void (*on_key)(int key))
 {
 	globals.wl_display = wl_display_connect(NULL);
 	globals.wl_registry = wl_display_get_registry(globals.wl_display);
 	wl_registry_add_listener(globals.wl_registry, &wl_registry_listener, NULL);
 	wl_display_roundtrip(globals.wl_display);
 
-	if (!globals.wl_compositor) log_fatal("Failed to bind wl_compositor");
-	if (!globals.wl_seat) log_fatal("Failed to bind wl_seat");
-	if (!globals.zwlr_layer_shell_v1) log_fatal("Failed to bind zwlr_layer_shell_v1");
-	if (!globals.wp_fractional_scale_manager_v1) log_fatal("Failed to bind wp_fractional_scale_manager_v1");
-	if (!globals.wp_viewporter) log_fatal("Failed to bind wp_viewporter");
+	if (!globals.wl_compositor)
+		log_fatal("Failed to bind wl_compositor");
+	if (!globals.wl_seat)
+		log_fatal("Failed to bind wl_seat");
+	if (!globals.zwlr_layer_shell_v1)
+		log_fatal("Failed to bind zwlr_layer_shell_v1");
+	if (!globals.wp_fractional_scale_manager_v1)
+		log_fatal("Failed to bind wp_fractional_scale_manager_v1");
+	if (!globals.wp_viewporter)
+		log_fatal("Failed to bind wp_viewporter");
 
 	window.wl_surface = wl_compositor_create_surface(globals.wl_compositor);
 
-	if (!window.wl_surface) log_fatal("Failed to create surface");
+	if (!window.wl_surface)
+		log_fatal("Failed to create surface");
 
 	window.width = 1920;
 	window.height = 1080;
@@ -228,21 +249,22 @@ void window_init(void (*on_draw)(), void (*on_resize)(int width, int height), vo
 
 void window_show()
 {
-	window.zwlr_layer_surface_v1 = zwlr_layer_shell_v1_get_layer_surface(
-			globals.zwlr_layer_shell_v1, window.wl_surface, NULL,
-			ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY, "gameshell");
+	window.zwlr_layer_surface_v1 =
+			zwlr_layer_shell_v1_get_layer_surface(globals.zwlr_layer_shell_v1, window.wl_surface,
+					NULL, ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY, "gameshell");
 
-	if (!window.zwlr_layer_surface_v1) log_fatal("Failed to create layer surface");
+	if (!window.zwlr_layer_surface_v1)
+		log_fatal("Failed to create layer surface");
 
-	zwlr_layer_surface_v1_add_listener(window.zwlr_layer_surface_v1, &zwlr_layer_surface_v1_listener, NULL);
+	zwlr_layer_surface_v1_add_listener(window.zwlr_layer_surface_v1,
+			&zwlr_layer_surface_v1_listener, NULL);
 	zwlr_layer_surface_v1_set_anchor(window.zwlr_layer_surface_v1,
 			ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP + ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM +
-			ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT + ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT);
+					ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT + ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT);
 	zwlr_layer_surface_v1_set_exclusive_zone(window.zwlr_layer_surface_v1, -1);
 
 	if (window.on_key) {
-		zwlr_layer_surface_v1_set_keyboard_interactivity(
-				window.zwlr_layer_surface_v1,
+		zwlr_layer_surface_v1_set_keyboard_interactivity(window.zwlr_layer_surface_v1,
 				ZWLR_LAYER_SURFACE_V1_KEYBOARD_INTERACTIVITY_EXCLUSIVE);
 
 		struct wl_keyboard *wl_keyboard = wl_seat_get_keyboard(globals.wl_seat);
@@ -278,7 +300,8 @@ int window_dispatch()
 
 void window_redraw()
 {
-	if (!window.visible) return;
+	if (!window.visible)
+		return;
 
 	if (window.on_draw)
 		window.on_draw();
